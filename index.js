0781314a7197c9cf2fb3e19dfc6b61941c3957fe
ahmedj <ahmedarifusa@gmail.com>
2024-05-09 15:53:39 +0300

    updated

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+
+
+var employee = {
+    name: "John Doe",
+    streetAddress: "123 Main Street"
+};
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+    };
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+
+function deleteFromEmployeeByKey(employee, key) {
+    var newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
